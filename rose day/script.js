c3e74d7fcf6604b1635c0bb835cd872a4734dd9a
Aditya Pandey <128928890+Aditya08pandey@users.noreply.github.com>
2025-02-06 23:37:02 +0530

    Delete rose day directory

--- rose day/script.js
+++
@@ -1,7 +0,0 @@
-function showRose() {
-    const rose = document.getElementById('rose');
-    const box = document.querySelector('.box');
-    
-    box.style.display = 'none';
-    rose.style.display = 'block';
-}
