031ed311b8c3ff428a1e7c30560a21860a1ca533
Aditya Pandey <128928890+Aditya08pandey@users.noreply.github.com>
2025-02-06 23:37:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showRose() {
+    const rose = document.getElementById('rose');
+    const box = document.querySelector('.box');
+    
+    box.style.display = 'none';
+    rose.style.display = 'block';
+}
